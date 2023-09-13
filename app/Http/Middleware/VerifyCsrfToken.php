620d0c15b4adac65a7499ed3ab233a712a0d3074
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-09-13 16:49:37 +0300

    Removing route '/user/login'

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         
-        "http://127.0.0.1:8000/user/login"
+        //"http://127.0.0.1:8000/user/login"
 
     ];
 }
