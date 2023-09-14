722047f4335f0241787dd5bd06b30762284f51bf
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-09-14 10:07:07 +0300

    Creating Recipe Model

---
+++ app/Models/Recipe.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "author",
+        "title",
+        "image"
+    ];
+
+    public function author() {
+
+        return $this->belongsTo(User::class);
+
+    }
+}
