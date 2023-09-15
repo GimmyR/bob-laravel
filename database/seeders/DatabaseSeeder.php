f9555c07364375d8c325d46b8fd32e293c7e8845
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-09-15 08:41:38 +0300

    Adding test data for Ingredient and Instruction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,5 +28,95 @@ class DatabaseSeeder extends Seeder
             "image" => "recipes/poulet-legumes.jpeg"
         ]);
 
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Cuisse de poulet"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Carotte"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Patates"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Courgette"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Poivron"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Oignon"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Carry"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Poivre"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Origan"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Huile d'olive"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Citron"
+        ]);
+
+        DB::table("ingredients")->insert([
+            "recipe_id" => 1,
+            "description" => "Piment"
+        ]);
+
+        DB::table("instructions")->insert([
+            "recipe_id" => 1,
+            "details" => "Couper en tranches la carotte, la courgette et les patates"
+        ]);
+
+        DB::table("instructions")->insert([
+            "recipe_id" => 1,
+            "details" => "Couper en petites tranches le poivron et l'oignon"
+        ]);
+
+        DB::table("instructions")->insert([
+            "recipe_id" => 1,
+            "details" => "Mettre la cuisse de poulet et les tranches de légumes dans une poêle"
+        ]);
+
+        DB::table("instructions")->insert([
+            "recipe_id" => 1,
+            "details" => "Ajouter 500ml d'eau, une cuillerée de carry, une cuillerée d'origan et une pincée de poivre "
+        ]);
+
+        DB::table("instructions")->insert([
+            "recipe_id" => 1,
+            "details" => "Mettre la poêle sur le feu avec un couvercle pendant 45min en veillant à ce qu'il y ait toujours de L'eau"
+        ]);
+
+        DB::table("instructions")->insert([
+            "recipe_id" => 1,
+            "details" => "Ajouter deux cuillerées d'huile d'olive, une cuillerée de purée de piment et un citron pressé"
+        ]);
+
     }
 }
