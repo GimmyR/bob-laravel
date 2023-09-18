e130ed6007372befc0654b079e2de6b1b5489c06
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-09-18 09:20:55 +0300

    Editing indentation space value

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.jsx')
-    @inertiaHead
-    <title>Bread on Board</title>
-  </head>
-  <body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        @vite('resources/js/app.jsx')
+        @inertiaHead
+        <title>Bread on Board</title>
+    </head>
+<body>
     @inertia
-  </body>
+</body>
 </html>
