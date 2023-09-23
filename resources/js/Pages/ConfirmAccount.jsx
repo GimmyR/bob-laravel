d9626b020b8bcea0df8559f74d6e644212074c4d
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-09-23 06:46:30 +0300

    Editing aesthetic

--- resources/js/Pages/ConfirmAccount.jsx
+++ resources/js/Pages/ConfirmAccount.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Layout from "../Layout";
+import { Alert, Col, Container, Row } from "react-bootstrap";
 
 const ConfirmAccount = function({ success, error }) {
 
     return (
-        <div>
-            <p>{success || error}</p>
-        </div>
+        <Container className="mt-3">
+            <Row className="d-flex flex-row justify-content-center">
+                <Col lg={6}>
+                    {success != null && <Alert variant="success">{success}</Alert>}
+                    {error != null && <Alert variant="danger">{error}</Alert>}
+                </Col>
+            </Row>
+        </Container>
     );
 
 };
